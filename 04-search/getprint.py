47e17f26492270b3abaacf3cffc21be726228037
Marek Perichta <mperichta@gmail.com>
2018-10-22 18:38:10 +0200

    init 04 added getprint

--- 04-search/getprint.py
+++ 04-search/getprint.py
@@ -2,28 +2,27 @@ import sqlite3
 import sys
 import json
 
+
 def create_connection(db_file):
     conn = sqlite3.connect(db_file)
     return conn
 
 
-def getPrint(print_number):
-    conn = sqlite3.connect("scorelib.dat")
+def get_print(conn, print_number):
     curs = conn.cursor()
     sql_query = 'SELECT person.name, person.born, person.died ' \
                 'FROM print ' \
-                'LEFT JOIN edition ON edition.id = print.edition ' \
-                'LEFT JOIN score_author ON score_author.score = edition.score ' \
-                'LEFT JOIN person ON score_author.composer = person.id ' \
+                'INNER JOIN edition ON edition.id = print.edition ' \
+                'INNER JOIN score_author ON score_author.score = edition.score ' \
+                'INNER JOIN person ON score_author.composer = person.id ' \
                 'WHERE print.id = ?'
-
-    curs.execute(sql_query, [print_number])
+    curs.execute(sql_query, (print_number,))
     rows = curs.fetchall()
     return rows
 
 print_number = sys.argv[1]
 conn = create_connection("scorelib.dat")
-results = getPrint(print_number)
+results = get_print(conn, print_number)
 
 list = []
 for result in results:
